e82cdc8081cb9afeb52083513f11cca9f0643a56
Md Johirul Islam <mdjohiruli826@gmail.com>
2026-06-10 23:56:50 +0600

    magical tree, matrix again and jadu matrix solve these problem

--- Magical_Tree.c
+++ Magical_Tree.c
@@ -1,18 +1,37 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int n;
     scanf("%d", &n);
-    int star=(n+10+1)/2;
-    int space=star-1;
-    for(int i=1; i<=space; i++)
+    int star = 1;
+    int length = (n + 10 + 1) / 2;
+    int space = length - 1;
+
+    for (int i = 1; i <= length; i++)
     {
-        printf(" ");
-        for(int j=1; j<star; j++)
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+        for (int s = 1; s <= star; s++)
         {
             printf("*");
         }
         space--;
-        star+=2;
+        star += 2;
+        printf("\n");
+    }
+
+    for(int i=1; i<=5; i++)
+    {
+        for(int s=1; s<=5; s++)
+        {
+        printf(" ");
+        }
+        for(int j=1; j<=n; j++)
+        {
+            printf("*");
+        }
         printf("\n");
     }
 }
