fa5121475607864991862d54e83b518afea86447
Md Johirul Islam <mdjohiruli826@gmail.com>
2026-06-20 01:05:45 +0600

    palindrome,sorting,lucky array,replace min max,shift zero,comparsion,mathematical exprecess

---
+++ M_Replace_MinMax.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    int min_index=0;
+    int max_index=0;
+
+    for(int i=1; i<n; i++)
+    {
+        if(a[i]<a[min_index])
+        {
+            min_index=i;
+        }
+        if(a[i]>a[max_index])
+        {
+            max_index=i;
+        }
+    }
+    swap(a[min_index],a[max_index]);
+    for(int i=0; i<n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+    return 0;
+}
