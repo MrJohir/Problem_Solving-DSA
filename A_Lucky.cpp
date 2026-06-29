6712e3dfa2703ac84004b2d459875ae92f1d601f
Md Johirul Islam <mdjohiruli826@gmail.com>
2026-06-30 00:36:02 +0600

    lucky this problem solve

--- A_Lucky.cpp
+++ A_Lucky.cpp
@@ -2,13 +2,29 @@
 using namespace std;
 int main()
 {
-    int test_case;
-    cin >> test_case;
-    for (int t = 1; t <= test_case; t++)
+    int t;
+    cin >> t;
+
+    while (t--)
     {
-        char s;
-        cin>>s;
-        cout<<(int)s;
+        string s;
+        cin >> s;
+        int first_sum = 0;
+        int second_sum = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            first_sum += s[i] - '0';
+        }
+        for (int i = 3; i < 6; i++)
+        {
+            second_sum += s[i] - '0';
+        }
+        if (first_sum == second_sum)
+            cout << "YES" << endl;
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
