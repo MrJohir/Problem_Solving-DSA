2f5ceb580964e41d86e7f6d38eddea14e81797d9
Md Johirul Islam <mdjohiruli826@gmail.com>
2026-07-01 00:22:07 +0600

    replace word this problem solved

--- V_Replace_Word.cpp
+++ V_Replace_Word.cpp
@@ -4,10 +4,16 @@ int main()
 {
     string s;
     cin>>s;
-    string st="EGYPT";
-    for(char ch:s)
+    for(int i=0; i<s.size(); i++)
     {
-        // if(st=ch)
+        if(s[i]=='E'&& s[i+1]=='G'&& s[i+2]=='Y'&& s[i+3]=='P'&& s[i+4]=='T')
+        {
+            cout<<" ";
+            i+=4;
+        }
+        else{
+            cout<< s[i];
+        }
     }
     return 0;
 }
